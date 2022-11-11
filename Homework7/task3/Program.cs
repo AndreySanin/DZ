1093ce75dca6904b74c385f1057728ade2492fb5
Andrey <sanin.213@gmail.com>
2022-11-11 15:30:55 +0300

    Homework 7.3 added

---
+++ Homework7/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+double[] AverageOfEachColumn(int[,] arr)
+{
+    double[] sums = new double[arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            sum=sum+arr[j,i];
+        }
+        sums[i] = Math.Round(sum/arr.GetLength(0),1);
+    }
+    return sums;
+}
+
+void PrintArray(double[] array) 
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[,] arr = {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
+PrintArray(AverageOfEachColumn(arr));
